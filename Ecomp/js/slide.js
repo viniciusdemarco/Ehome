40c6103e7e7cc33f6c2641849588c6f006e2a602
Vinícius Eduardo Martins de Marco <103152547+viniciusdemarco@users.noreply.github.com>
2025-03-26 20:10:04 -0300

    Add files via upload

---
+++ Ecomp/js/slide.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slide1').slick({
+      slidesToShow: 1,
+      dots: true,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 1000,
+    });
+    $('.slide2').slick({
+      slidesToShow: 1,
+      dots: true,
+      arrows: true,
+    });
+  });
